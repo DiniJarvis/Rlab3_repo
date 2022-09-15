3a2b7e6ed45796e9d35612c7f4515c9a51d33560
DiniJarvis <dinesh.sundar2006@gmail.com>
2022-09-16 00:02:52 +0200

    check

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,7 +6,7 @@
 # * https://r-pkgs.org/tests.html
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
-library(testthat)
+#library(testthat)
 library(Rlab3pack)
 
 test_check("Rlab3pack")
